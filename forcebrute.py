eefa9a108e791d38ee461384a77940f83f30cf71
ThiveyaSellar <sellarthiveya@gmail.com>
2023-07-31 21:39:11 +0200

    Optimisation du code

--- forcebrute.py
+++ forcebrute.py
@@ -1,113 +1,54 @@
-"""
-Le programme doit explorer toutes les combinaisons
-"""
-
 import csv
+import sys
 import time
-import math
 
-MAX_BUDGET = 500
+import combinations as cb
 
+MAX_BUDGET = float(sys.argv[2])
 
-def show_combinations_number(nb_actions):
-    """
-    Calculer le nombre de combinaisons possibles 
-    à partir d'un nombre d'actions
-    """
-    somme = 0
-    for k in range(0,nb_actions):
-        somme += math.factorial(nb_actions)/(math.factorial(k) * math.factorial(nb_actions-k))
-    print(f"Nombre d'actions : {nb_actions}")
-    print(f"Nombre total de combinaisons possibles : {int(somme)}")
-
-def get_combinations_of_k_elements(elements, k, combinations, start=0, combination=[]):
-    """
-    Récupérer toutes les combinaisons de k actions parmi la liste des actions
-    """
-    if len(combination) == k:
-        combinations.append(list(combination))
-        return 
-    for i in range(start, len(elements)):
-        combination.append(elements[i])
-        get_combinations_of_k_elements(elements, k, combinations, i + 1, combination)
-        combination.pop()
-
-def get_all_combinations(actions, combinations):
-    """
-    Récupérer toutes les combinaisons possibles de k actions
-    avec 1 <= k <= n, n étant le nombre total d'actions 
-    """
-    for k in range(1,len(actions)+1):
-        get_combinations_of_k_elements(actions, k, combinations)
-    print(f"Nombre total trouvé: {len(combinations)}")
-
-def get_all_combinations_in_budget(combinations, combinations_in_budget):
-    """
-    Récupérer parmi une liste de combinaisons
-    les combinaisons où la somme totale des coûts des actions
-    ne dépasse pas le budget max.
-    """
-    for combination in combinations:
-        cost = 0
-        for action in combination:
-            cost += action[1]
-        if cost <= MAX_BUDGET:
-            combinations_in_budget.append(combination)
-    print(f"Nombre total dans le budget : {len(combinations_in_budget)}")
-
-def calculate_combinations_profit(combinations_in_budget):
-    """
-    Calculer le profit total de chaque combinaison.
-    """
-    for combination in combinations_in_budget:
-        profit = 0
-        for action in combination:
-            profit += action[1] * action[2]/100
-        combination.append(profit)
-
-def show_combination_details(combination):
-    """
-    Afficher pour une combinaison:
-    - la liste des actions
-    - le coût total
-    - le profit
-    """
-    total_cost = 0
-    for action in combination[:-1]:
-        print(f"{action[0]}, coût : {action[1]}")
-        total_cost += action[1]
-    print(f"Coût total : {total_cost}")
-    print(f"Profit : {combination[-1]}")
+filename = sys.argv[1]
 
 start = time.time()
-with open('forcebrute_dataset.csv', newline='') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    
-    actions = []
-    # Skip the header
-    header = next(reader)
-    for row in reader:
-        row[1] = float(row[1])
-        row[2] = float(row[2])
-        actions.append(row)
-    nb_actions = len(actions)
 
-    show_combinations_number(nb_actions)
+actions = cb.get_actions_from_csv(filename)
+nb_actions = len(actions)
 
-    combinations = []
-    get_all_combinations(actions, combinations)
-    
-    combinations_in_budget = []
-    get_all_combinations_in_budget(combinations, combinations_in_budget)
+cb.show_combinations_number(nb_actions)
 
-    calculate_combinations_profit(combinations_in_budget)
+combinations = []
+cb.get_all_combinations(actions, combinations)
 
-    combinations_in_budget.sort(key=lambda x: x[-1], reverse=True)
-    show_combination_details(combinations_in_budget[0])
-    
+combinations_in_budget = []
+cb.get_all_combinations_in_budget(combinations, combinations_in_budget, MAX_BUDGET)
 
-    end = time.time()
-    print(f"Temps: {end - start}")
+cb.calculate_combinations_profit(combinations_in_budget)
+
+combinations_in_budget.sort(key=lambda x: x[-1], reverse=True)
+cb.show_combination_details(combinations_in_budget[0])
+
+cb.execution_time(start)
 
 # Vérifier les floats 
 # Intéressant quand on transsforme les données en float dès le début on passe de 14 à 7 secondes
+# Nom du fichier sur ligne de commande
+# Vérifier si header ?
+
+# Force brute : test de toutes les combinaisons possibles
+# Commentaire sur le temps que ça prends et que si nombre d'actions plus longue
+# Risque de prendre beaucoup plus de temps
+
+# Logique de l'algo optimisé
+# Pas toutes les combinaisons
+# Trie de la liste par rapport au bénéfice
+# Récupère les actions pourcentage de bééfice éléevé et rentre dans le budget
+# Mais au bout d'un moment il ne fait pas attention au pourcentage le plus intéressant 
+# Peut être donner l'exemple
+# Il donne le meilleur relativement même prix mais légère perte de bénéfice relativement mais gain de temps
+# Présenter et analyser
+# Big O force brute :
+# Big O optimisé :
+
+# Partie 3
+# Télécharger les fichiers
+# Corriger 
+# Comparer résultats avec exécution en optimisé
--- optimized.py
+++ optimized.py
@@ -1,66 +1,42 @@
-# Le programme ne doit plus explorer toutes les combinaisons 
-
 import csv
+import sys
 import time
-import math
 
-MAX_BUDGET = 500
+import combinations as cb
 
-def get_combinations_of_k_elements(elements, k, combinations, start=0, combination=[]):
-    if len(combination) == k:
-        combinations.append(list(combination))
-        return 
-    for i in range(start, len(elements)):
-        combination.append(elements[i])
-        get_combinations_of_k_elements(elements, k, combinations, i + 1, combination)
-        combination.pop()
+MAX_BUDGET = float(sys.argv[2])
 
-def get_all_combinations(actions, combinations):
-    # Les actions sont listées à partir du deuxième élément [1:]
-    for k in range(len(actions)+1,1,-1):
-        get_combinations_of_k_elements(actions, k, combinations)
-
-    print(f"Nombre total trouvé: {len(combinations)}")
+filename = sys.argv[1]
 
 start = time.time()
-with open('forcebrute_dataset.csv', newline='') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
+
+actions = cb.get_actions_from_csv(filename)
+nb_actions = len(actions)
+
+for action in actions:
+    action.append(action[1]*action[2]/100)
+
+# Trier par le profit
+actions.sort(key=lambda x: x[2],reverse=True)
+
+i = 0
+total_cost = 0
+total_profit = 0
+selected_actions = []
+
+while total_cost <= MAX_BUDGET and i <= nb_actions-1:
+    if total_cost + actions[i][1] <= MAX_BUDGET:
+        total_cost += actions[i][1]
+        total_profit += actions[i][3]
+        selected_actions.append(actions[i])
+    i += 1
+        
+print(f"Nombre d'actions au total : {nb_actions}")
+print(f"Nombre d'actions sélectionnées au total : {len(selected_actions)}")
+for action in selected_actions:
+    print(action)
+print(f"Coût total : {total_cost}")
+print(f"Profit total : {total_profit}")
+
+cb.execution_time(start)
     
-    actions = []
-    next(reader)
-    for row in reader:
-        row[1] = float(row[1])
-        row[2] = float(row[2])
-        actions.append(row)
-    nb_actions = len(actions)
-    for action in actions:
-        action.append(float(action[1]) * float(action[2])/100)
-
-    # Sort by profit column
-    actions.sort(key=lambda x: x[2],reverse=True)
-    print("--------Liste des actions---------")
-
-    for action in actions:
-        print(action)
-
-    print("--------Meilleure liste des actions---------")
-
-    total_cost = 0
-    total_profit = 0
-    i = 0
-    selected_actions = []
-    while total_cost <= MAX_BUDGET and i <= nb_actions-1:
-        if total_cost + actions[i][1] <= MAX_BUDGET:
-            total_cost += actions[i][1]
-            total_profit += actions[i][3]
-            selected_actions.append(actions[i])
-            i += 1
-        else:
-            i += 1
-            
-    for action in selected_actions:
-        print(action)
-    print(f"Coût total : {total_cost}")
-    print(f"Profit total : {total_profit}")
-    end = time.time()
-    print(f"Temps: {end - start}")
