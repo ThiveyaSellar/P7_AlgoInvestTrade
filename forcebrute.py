f3943eac9e59b106d3c91c15836c18c9358e5c7c
ThiveyaSellar <sellarthiveya@gmail.com>
2023-08-08 09:28:31 +0200

    Modification algorithme forcebrute

--- forcebrute.py
+++ forcebrute.py
@@ -27,28 +27,3 @@ combinations_in_budget.sort(key=lambda x: x[-1], reverse=True)
 cb.show_combination_details(combinations_in_budget[0])
 
 cb.execution_time(start)
-
-# Vérifier les floats 
-# Intéressant quand on transsforme les données en float dès le début on passe de 14 à 7 secondes
-# Nom du fichier sur ligne de commande
-# Vérifier si header ?
-
-# Force brute : test de toutes les combinaisons possibles
-# Commentaire sur le temps que ça prends et que si nombre d'actions plus longue
-# Risque de prendre beaucoup plus de temps
-
-# Logique de l'algo optimisé
-# Pas toutes les combinaisons
-# Trie de la liste par rapport au bénéfice
-# Récupère les actions pourcentage de bééfice éléevé et rentre dans le budget
-# Mais au bout d'un moment il ne fait pas attention au pourcentage le plus intéressant 
-# Peut être donner l'exemple
-# Il donne le meilleur relativement même prix mais légère perte de bénéfice relativement mais gain de temps
-# Présenter et analyser
-# Big O force brute :
-# Big O optimisé :
-
-# Partie 3
-# Télécharger les fichiers
-# Corriger 
-# Comparer résultats avec exécution en optimisé
